b11a2dbaf9c012bc9c1e7935dc7a33700fe7c15b
Charles Crain <craincharles@gmail.com>
2019-07-05 14:49:23 -0700

    PixuraFungibleToken as erc20 + metadata contract

---
+++ truffle/contracts/PixuraFungibleToken.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract PixuraFungibleToken is ERC20, ERC20Detailed, Ownable {
+    using SafeMath for uint256;
+
+    // String uri holding the metadata for the token.
+    string public _uri;
+
+    event TokenURIUpdated(string indexed tokenURI);
+
+    constructor(
+      string memory name,
+      string memory symbol,
+      uint8 decimals,
+      string memory uri
+    )
+    ERC20Detailed(name, symbol, decimals)
+    {
+        _uri = uri;
+    }
+
+    /**
+     * @dev Update the token metadata URI.
+     * @return string
+     */
+    function tokenURI() external view returns (string) {
+        return _uri;
+    }
+
+    /**
+     * @dev Update the token metadata URI.
+     * @param uri string new uri.
+     */
+    function updateTokenURI(string memory uri) public onlyOwner {
+        emit TokenURIUpdated(uri);
+        _uri = uri;
+    }
+
+}
