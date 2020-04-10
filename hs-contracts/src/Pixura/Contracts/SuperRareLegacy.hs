292dab377b306744780d332e7ba20a4c73e284b9
Charles Crain <craincharles@gmail.com>
2020-04-10 09:20:04 -0700

    legacy hs bindings

---
+++ hs-contracts/src/Pixura/Contracts/SuperRareLegacy.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+
+module Pixura.Contracts.SuperRareLegacy where
+
+import           Network.Ethereum.Contract.TH
+
+[abiFrom|contracts/v5/build/SuperRareLegacy.json|]
